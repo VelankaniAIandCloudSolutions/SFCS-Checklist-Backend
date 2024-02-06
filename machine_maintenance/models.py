6d6ebef196f08d9c529f8990a9825fc02680d53f
satvik <katochsatvik@gmail.com>
2024-02-06 17:35:46 +0530

    partial db-schema

--- machine_maintenance/models.py
+++ machine_maintenance/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from accounts.models import BaseModel
+from django.utils import timezone
 # Create your models here.
+
+
+class Line(BaseModel):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Machine(BaseModel):
+    name = models.CharField(max_length=255)
+
+    line = models.ForeignKey(Line, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Model(BaseModel):
+    name = models.CharField(max_length=255)
+    machine = models.ForeignKey(Machine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class MaintenancePlan(BaseModel):
+
+    maintenance_date = models.DateField(default=timezone.now)
