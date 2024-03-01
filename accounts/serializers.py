5739ea9e2f8e1c999d38fcaaf5139385b7500522
satvik <katochsatvik@gmail.com>
2024-03-01 15:37:48 +0530

    role permissions added

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -10,7 +10,7 @@ class UserCreateSerializer(djoser_serializers.UserCreateSerializer):
     class Meta(djoser_serializers.UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'password',
-                  'phone_number', 'is_superuser', 'is_staff', 'is_store_team', 'is_design_team')
+                  'phone_number', 'is_superuser', 'is_staff', 'is_store_team', 'is_design_team', 'is_machine_maintenance_supervisor_team', 'is_machine_maintenance_staff_team')
 
 
 class UserAccountSerializer(serializers.ModelSerializer):
