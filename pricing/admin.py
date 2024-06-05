e8ea91449dbd7577e54c671b860dc6011cd7299c
satvik <katochsatvik@gmail.com>
2024-06-05 16:44:45 +0530

    created the tables

--- pricing/admin.py
+++ pricing/admin.py
@@ -3,3 +3,12 @@ from .models import *
 # Register your models here.
 admin.site.register(PartPricing)
 admin.site.register(AccessToken)
+
+
+admin.site.register(Manufacturer)
+admin.site.register(ManufacturerPart)
+admin.site.register(Currency)
+admin.site.register(PackageType)
+admin.site.register(ManufacturerPartDistributorDetail)
+admin.site.register(DistributorPackageTypeDetail)
+admin.site.register(ManufacturerPartPricing)
--- pricing/serializers.py
+++ pricing/serializers.py
@@ -1,12 +1,69 @@
 from rest_framework import serializers
 from .models import PartPricing
-from store_checklist.serializers import ProjectListSerializer,ProductSerializer
+from accounts.serializers import UserAccountSerializer
+from store_checklist.serializers import ProjectListSerializer, ProductSerializer
+from pricing.models import *
+
 
 class PartPricingSerializer(serializers.ModelSerializer):
     project = ProjectListSerializer()
     product = ProductSerializer()
+
     class Meta:
         model = PartPricing
         fields = '__all__'
 
-    
+
+class CurrencySerializer(serializers.ModelSerializer):
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    updated_by = UserAccountSerializer()
+    created_by = UserAccountSerializer()
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+
+    class Meta:
+        model = Currency
+        fields = '__all__'
+
+
+class PackageTypeSerializer(serializers.ModelSerializer):
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    updated_by = UserAccountSerializer()
+    created_by = UserAccountSerializer()
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+
+    class Meta:
+        model = PackageType
+        fields = '__all__'
+
+
+class ManufacturerPartDistributorDetailSerializer(serializers.ModelSerializer):
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    updated_by = UserAccountSerializer()
+    created_by = UserAccountSerializer()
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+
+    class Meta:
+        model = ManufacturerPartDistributorDetail
+        fields = '__all__'
+
+
+class DistributorPackageTypeDetailSerializer(serializers.ModelSerializer):
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    updated_by = UserAccountSerializer()
+    created_by = UserAccountSerializer()
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+
+    class Meta:
+        model = DistributorPackageTypeDetail
+        fields = '__all__'
+
+
+class ManufacturerPartPricingSerializer(serializers.ModelSerializer):
+    updated_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+    updated_by = UserAccountSerializer()
+    created_by = UserAccountSerializer()
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S')
+
+    class Meta:
+        model = ManufacturerPartPricing
+        fields = '__all__'
